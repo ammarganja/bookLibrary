b1bc5ccaf017d3da4a99ee8750266b1763d14f17
ammarganja <ammarganja17@gmail.com>
2020-03-17 19:04:04 +0530

    0

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ function Book(name, author, type, number) {
 
 // store in localstorage
 
-window.localStorage.setItem('book', '');
+// window.localStorage.setItem('book', '');
 
 // Display Construtor
 
@@ -175,26 +175,6 @@ function searchdata() {
     table = document.getElementById("tabelbody");
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
-    }
-}
-
-
-function searchdata() {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("input-data");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("tabelbody");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[1];
         if (td) {
             txtValue = td.textContent || td.innerText;
@@ -207,43 +187,7 @@ function searchdata() {
     }
 }
 
-function searchdata() {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("input-data");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("tabelbody");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[2];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
-    }
-}
 
-function searchdata() {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("input-data");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("tabelbody");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[3];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
-    }
-}
 
 const search = document.getElementById("search-btn");
 search.addEventListener("click", searchdata);
